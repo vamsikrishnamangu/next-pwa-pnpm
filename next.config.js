02bc8a7a53c6b68ea1f3f671d35b1e18dc8266e1
vamsikrishnamangu <mvvk96@gmail.com>
2023-09-27 11:31:54 +0530

    feat: pnpm commit with next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const pwaConfig = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
+
+module.exports = {
+  ...nextConfig,
+  ...pwaConfig,
+};
